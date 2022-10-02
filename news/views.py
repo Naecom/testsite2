70dcd9aa0bc86f6c21f973a36482bd9040d94f2b
Naecom <mgusmanov@gmail.com>
2022-10-02 22:51:58 +0600

    1

---
+++ news/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News
+
+# Create your views here.
+def index(request):
+    news = News.objects.order_by('-created_at')
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+
+def test(request):
+    return HttpResponse("<h1>test page</h1>")
+ 
